fca46bd4fc541ec53297597d92a488799cbcc0f3
Maxim Brett <maxim.brett.ru@gmail.com>
2026-02-18 23:20:02 +0400

    initional commit

---
+++ src/test/java/bank/app/springbootbankapp/SpringBootBankAppApplicationTests.java
@@ -0,0 +1,13 @@
+package bank.app.springbootbankapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootBankAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
